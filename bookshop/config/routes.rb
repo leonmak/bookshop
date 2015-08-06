fb6dde98273cc43b415d00fa606af2a291ad70a3
leon mak <leonmakansheng@gmail.com>
2015-08-06 20:14:17 +0800

    First Fucking sale

--- bookshop/config/routes.rb
+++ bookshop/config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'dashboard'  => 'pages#dashboard'
+#create transactions
+  post '/buy/:slug', to: 'transactions#create', as: :buy
+#download book
+  get '/pickup/:guid', to: 'transactions#pickup', as: :pickup
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
